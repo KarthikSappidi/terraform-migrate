10abcc6474e5d55902bc490e3c0564221a31284d
karthiksappidi <karthiksappidi@gmail.com>
2024-04-18 15:59:40 +0530

    import

---
+++ migrate/main.tf
@@ -0,0 +1,80 @@
+# __generated__ by Terraform
+# Please review these resources and move them into your main configuration files.
+
+# __generated__ by Terraform
+resource "aws_instance" "myinstance" {
+  ami                                  = "ami-007020fd9c84e18c7"
+  associate_public_ip_address          = true
+  availability_zone                    = "ap-south-1b"
+  disable_api_stop                     = false
+  disable_api_termination              = false
+  ebs_optimized                        = false
+  get_password_data                    = false
+  hibernation                          = false
+  host_id                              = null
+  host_resource_group_arn              = null
+  iam_instance_profile                 = null
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t2.micro"
+  key_name                             = "karthik"
+  monitoring                           = false
+  placement_group                      = null
+  placement_partition_number           = 0
+  private_ip                           = "172.31.11.125"
+  secondary_private_ips                = []
+  security_groups                      = ["launch-wizard-10"]
+  source_dest_check                    = true
+  subnet_id                            = "subnet-052cbf3a2ff6ce075"
+  tags = {
+    Name = "tf-migrate"
+  }
+  tags_all = {
+    Name = "tf-migrate"
+  }
+  tenancy                     = "default"
+  user_data                   = null
+  user_data_base64            = null
+  user_data_replace_on_change = null
+  volume_tags                 = null
+  vpc_security_group_ids      = ["sg-0e00ef89f966f2e4d"]
+  capacity_reservation_specification {
+    capacity_reservation_preference = "open"
+  }
+  cpu_options {
+    amd_sev_snp      = null
+    core_count       = 1
+    threads_per_core = 1
+  }
+  credit_specification {
+    cpu_credits = "standard"
+  }
+  enclave_options {
+    enabled = false
+  }
+  maintenance_options {
+    auto_recovery = "default"
+  }
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_protocol_ipv6          = "disabled"
+    http_put_response_hop_limit = 2
+    http_tokens                 = "required"
+    instance_metadata_tags      = "disabled"
+  }
+  private_dns_name_options {
+    enable_resource_name_dns_a_record    = true
+    enable_resource_name_dns_aaaa_record = false
+    hostname_type                        = "ip-name"
+  }
+  root_block_device {
+    delete_on_termination = true
+    encrypted             = false
+    iops                  = 100
+    kms_key_id            = null
+    tags                  = {}
+    tags_all              = {}
+    throughput            = 0
+    volume_size           = 8
+    volume_type           = "gp2"
+  }
+}
